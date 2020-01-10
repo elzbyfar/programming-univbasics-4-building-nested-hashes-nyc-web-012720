34c958c027e752c602adfa9621b17b5824a1ef83
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-10 03:54:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4, 
       :rent_in_dollars => {
-        :reading => 250, 
+        :names => "", 
         :pennsylvania => 250, 
         :monaco => 250, 
         :miami => 250
